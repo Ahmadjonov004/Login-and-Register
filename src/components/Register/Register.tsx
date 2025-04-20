f35ad953f87e3cc3605340d35a3ff42863d4be8e
ahmadjonov <ahmadjonovasilbek624@gmail.com>
2025-04-20 18:41:00 +0500

    components optimized

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
+import loginImg from "../../assets/images/loginImg.png";
 
 const Register: React.FC = () => {
   const [form, setForm] = useState({
-    first_name: '',
-    last_name: '',
-    username: '',
-    email: '',
-    password: '',
+    first_name: "",
+    last_name: "",
+    username: "",
+    email: "",
+    password: "",
   });
 
-  const [message, setMessage] = useState('');
-  const [error, setError] = useState('');
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -21,90 +22,131 @@ const Register: React.FC = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post("https://onlyauth.pythonanywhere.com/auth/register/", form);
+      const response = await axios.post(
+        "https://onlyauth.pythonanywhere.com/auth/register/",
+        form
+      );
       setMessage("Muvaffaqiyatli ro'yxatdan o'tdingiz");
-      setError('');
+      setError("");
       console.log("Success:", response.data);
     } catch (err: any) {
       setError("Ro'yxatdan o‘tishda xatolik yuz berdi");
-      setMessage('');
+      setMessage("");
       console.error("Error:", err.response?.data || err.message);
     }
   };
 
   return (
-    <div className="w-full flex items-center justify-center min-h-screen">
-      <form
-        onSubmit={handleSubmit}
-        className="w-full max-w-md p-6 bg-white rounded-2xl shadow-2xl space-y-4"
-      >
-        <h2 className="text-3xl font-bold text-center text-gray-800">
-          Ro‘yxatdan o‘tish
-        </h2>
-        <p className="text-center text-gray-500 text-sm">
-          Kerakli maʼlumotlarni kiriting
-        </p>
-
-        <input
-          type="text"
-          name="first_name"
-          placeholder="Ismingiz"
-          value={form.first_name}
-          onChange={handleChange}
-          required
-          className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-
-        <input
-          type="text"
-          name="last_name"
-          placeholder="Familiyangiz"
-          value={form.last_name}
-          onChange={handleChange}
-          required
-          className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={form.username}
-          onChange={handleChange}
-          required
-          className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={form.email}
-          onChange={handleChange}
-          required
-          className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-
-        <input
-          type="password"
-          name="password"
-          placeholder="Parol"
-          value={form.password}
-          onChange={handleChange}
-          required
-          className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-
-        {message && <p className="text-green-600 text-sm text-center">{message}</p>}
-        {error && <p className="text-red-500 text-sm text-center">{error}</p>}
-
-        <button
-          type="submit"
-          className="w-full py-2 bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-white rounded-xl font-semibold transition"
+    <div className="flex min-h-screen realtive">
+      <div className="md:w-1/2 w-full flex items-center justify-center p-3 md:p-6">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-md p-6 bg-white rounded-2xl shadow-2xl space-y-4"
         >
-          Ro‘yxatdan o‘tish
-        </button>
-      </form>
+          <h2 className="text-3xl font-bold text-center text-gray-800">
+            Ro‘yxatdan o‘tish
+          </h2>
+          <p className="text-center text-gray-500 text-sm">
+            Kerakli maʼlumotlarni kiriting
+          </p>
+          <div className="mb-[15px] md:mb-[20px]">
+            <label className="flex justify-start text-[16px] md:text-[18px] font-medium text-[#252525] mb-[9px]">
+              Ismingiz
+            </label>
+            <input
+              type="text"
+              name="first_name"
+              placeholder="Ismingiz"
+              value={form.first_name}
+              onChange={handleChange}
+              required
+              className="w-full px-4 py-[10px] md:py-[12px] border-none text-[16px] font-normal bg-[#F9F8FA] rounded-xl focus:ring-2 focus:ring-blue-500 focus:outline-none transition"
+            />
+          </div>
+
+          <div className="mb-[15px] md:mb-[20px]">
+            <label className="flex justify-start text-[16px] md:text-[18px] font-medium text-[#252525] mb-[9px]">
+              Familiyangiz
+            </label>
+            <input
+              type="text"
+              name="last_name"
+              placeholder="Familiyangiz"
+              value={form.last_name}
+              onChange={handleChange}
+              required
+              className="w-full px-4 py-[10px] md:py-[12px] border-none text-[16px] font-normal bg-[#F9F8FA] rounded-xl focus:ring-2 focus:ring-blue-500 focus:outline-none transition"
+            />
+          </div>
+          <div className="flex justify-center items-center gap-[20px]">
+            
+            <div className="">
+              <label className="flex justify-start text-[16px] md:text-[18px] font-medium text-[#252525] mb-[9px]">
+                Username
+              </label>
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={form.username}
+                onChange={handleChange}
+                required
+                className="w-full px-4 py-[10px] md:py-[12px] border-none text-[16px] font-normal bg-[#F9F8FA] rounded-xl focus:ring-2 focus:ring-blue-500 focus:outline-none transition"
+              />
+
+            </div>
+            <div className="">
+              <label className="flex justify-start text-[16px] md:text-[18px] font-medium text-[#252525] mb-[9px]">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={form.email}
+                onChange={handleChange}
+                required
+                className="w-full px-4 py-[10px] md:py-[12px] border-none text-[16px] font-normal bg-[#F9F8FA] rounded-xl focus:ring-2 focus:ring-blue-500 focus:outline-none transition"
+              />
+            </div>
+          </div>
+
+          <div className="mb-[15px] md:mb-[20px]">
+            <label className="flex justify-start text-[16px] md:text-[18px] font-medium text-[#252525] mb-[9px]">
+              Parol
+            </label>
+            <input
+            type="password"
+            name="password"
+            placeholder="Parol"
+            value={form.password}
+            onChange={handleChange}
+            required
+            className="w-full px-4 py-[10px] md:py-[12px] border-none text-[16px] font-normal bg-[#F9F8FA] rounded-xl focus:ring-2 focus:ring-blue-500 focus:outline-none transition"
+          />
+          </div>
+         
+
+          {message && (
+            <p className="text-green-600 text-sm text-center">{message}</p>
+          )}
+          {error && <p className="text-red-500 text-sm text-center">{error}</p>}
+
+          <button
+            type="submit"
+            className="w-full py-2 bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-white rounded-xl font-semibold transition"
+          >
+            Ro‘yxatdan o‘tish
+          </button>
+        </form>
+        <div className="hidden md:block w-full max-w-[600px] absolute  top-[50px] right-[50px] bottom-[50px] z-9 ">
+          <img
+            src={loginImg}
+            alt="Login"
+            className="object-cover w-full h-64 md:h-full"
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import loginImg from '../../assets/images/loginImg.png'; 
 
 const Login: React.FC = () => {
   const [form, setForm] = useState({ login: '', password: '' });
@@ -16,46 +17,72 @@ const Login: React.FC = () => {
         username: form.login,
         password: form.password,
       });
-      console.log('Token:', response.data);
       localStorage.setItem('access', response.data.access);
       localStorage.setItem('refresh', response.data.refresh);
       setError('');
+
     } catch (err) {
-      setError('Login yoki parol noto‘g‘ri');
+      setError("Login yoki parol noto‘g‘ri");
     }
   };
 
   return (
-    <div className="w-full flex items-center justify-center min-h-screen  bg-white">
-      <form onSubmit={handleSubmit} className="w-full max-w-sm p-6 bg-white rounded-2xl shadow-xl space-y-4">
-        <h2 className="text-3xl font-bold text-center">Xush kelibsiz!</h2>
-        <p className="text-center text-gray-500 text-sm">Login parolingizni kiriting</p>
-        <input
-          type="text"
-          name="login"
-          placeholder="Loginingizni kiriting"
-          value={form.login}
-          onChange={handleChange}
-          className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Parolingizni kiriting"
-          value={form.password}
-          onChange={handleChange}
-          className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
-          required
-        />
-        {error && <p className="text-red-500 text-sm">{error}</p>}
-        <button
-          type="submit"
-          className="w-full py-2 bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-white rounded-xl font-semibold transition"
+    <div className="flex min-h-screen realtive">
+      
+      <div className="md:w-1/2 w-full flex items-center justify-center p-3 md:p-6 ">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-md  p-4 md:p-8 rounded-2xl shadow-lg relative z-10"
         >
-          Kirish
-        </button>
-      </form>
+          <h2 className="text-3xl md:text-5xl font-bold text-center text-[#252525]">Xush kelibsiz!</h2>
+          <p className="text-center text-[#4B4B4B] text-[14px] md:text-[16px] font-normal mb-8 md:mb-[50px]">
+            Login parolingizni kiriting o‘z kabinetingizga kiring.
+          </p>
+
+          <div className='mb-[15px] md:mb-[20px]'>
+            <label className="flex justify-start text-[16px] md:text-[18px] font-medium text-[#252525] mb-[9px]">Login</label>
+            <input
+              type="text"
+              name="login"
+              value={form.login}
+              onChange={handleChange}
+              placeholder="Loginingizni kiriting"
+              required
+              className="w-full px-4 py-[10px] md:py-[12px] border-none text-[16px] font-normal bg-[#F9F8FA] rounded-xl focus:ring-2 focus:ring-blue-500 focus:outline-none transition"
+            />
+          </div>
+
+          <div className='mb-[30px] md:mb-[40px]'>
+            <label className="flex justify-start text-[16px] md:text-[18px] font-medium text-[#252525] mb-[9px]">Parol</label>
+            <input
+              type="password"
+              name="password"
+              value={form.password}
+              onChange={handleChange}
+              placeholder="Parolingizni kiriting"
+              required
+              className="w-full px-4 py-[10px] md:py-[12px] border-none text-[16px] font-normal bg-[#F9F8FA] rounded-xl focus:ring-2 focus:ring-blue-500 focus:outline-none transition"
+
+            />
+          </div>
+
+          {error && <p className="text-red-500 text-sm">{error}</p>}
+
+          <button
+            type="submit"
+            className="w-full py-2 bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-white rounded-xl font-semibold transition"
+          >
+            Kirish
+          </button>
+        </form>
+      </div>
+      <div className="hidden md:block w-full max-w-[600px] absolute  top-[50px] right-[50px] bottom-[50px] z-9 ">
+        <img
+          src={loginImg}
+          alt="Login"
+          className="object-cover w-full h-64 md:h-full"
+        />
+      </div>
     </div>
   );
 };
